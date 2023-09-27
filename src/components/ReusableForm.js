9353eed9da9692b13c2828f08545c67946069281
AndrewMMicke <andrew.m.mickel@gmail.com>
2023-09-27 11:28:58 -0700

    change desc to a textarea format + add labels

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -6,31 +6,35 @@ function ReusableForm(props) {
     return (
         <React.Fragment>
             <form onSubmit={props.formSubmissionHandler}>
+                <label for='imageurl'>Image url</label><br />
                 <input
                     type='text'
                     name='imageurl'
                     placeholder='URL to image'
-                    defaultValue={item ? item.imageurl : ""} />
+                    defaultValue={item ? item.imageurl : ""} /><br />
+                <label for='name'>Product Name</label><br />
                 <input
                     type='text'
                     name='name'
                     placeholder='Product Name'
-                    defaultValue={item ? item.name : ""} />
+                    defaultValue={item ? item.name : ""} /><br />
+                <label for='quantity'>Quantity</label><br />
                 <input
                     type='number'
                     name='quantity'
                     placeholder='Amount'
-                    defaultValue={item ? item.quantity : ""} />
+                    defaultValue={item ? item.quantity : ""} /><br />
+                <label for='price'>Price in Yen</label><br />
                 <input
                     type='number'
                     name='price'
                     placeholder='Price'
-                    defaultValue={item ? item.price : ""} />
-                <input
-                    type='text'
+                    defaultValue={item ? item.price : ""} /><br />
+                <label for='description'>Description</label><br />
+                <textarea
                     name='description'
                     placeholder='Description'
-                    defaultValue={item ? item.description : ""} />
+                    defaultValue={item ? item.description : ""} /><br />
                 <button type='submit'>{props.buttonText}</button>
             </form>
         </React.Fragment>
