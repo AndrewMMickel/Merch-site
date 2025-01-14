ce9248ca7b3f5634b09ab3b33fc6d7d9fb4afdfa
AndrewMMicke <andrew.m.mickel@gmail.com>
2025-01-14 11:59:16 -0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import reducer from './reducers/ItemList-reducer';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 store.subscribe(() =>
   console.log(store.getState())
